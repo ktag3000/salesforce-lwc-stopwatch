0e50227d782023382f6e422a466eb31f74ea2dd4
ktag3000 <136417976+ktag3000@users.noreply.github.com>
2025-06-14 08:12:10 -0500

    Created repo

---
+++ force-app/main/default/lwc/stopwatchUtilityBar/stopwatchUtilityBar.js
@@ -0,0 +1,52 @@
+import { LightningElement } from 'lwc';
+
+export default class StopWatch extends LightningElement {
+
+    timer = '00:00:00'
+    timerRef
+    isRunning = false
+    
+    actionHandler(event){
+        const {label} = event.target
+        if(label === 'Start'){
+            this.setTimer()
+        }
+        if(label === 'Stop'){
+            window.clearInterval(this.timerRef)
+            window.localStorage.removeItem('startTimer')
+        }
+        if(label === 'Reset'){
+            this.timer='00:00:00'
+            window.clearInterval(this.timerRef)
+            window.localStorage.removeItem('startTimer')
+        }
+        
+    }
+    StartTimerHandler(){
+        const startTime = new Date()
+        window.localStorage.setItem('startTimer', startTime)
+        return startTime
+    }
+    
+    setTimer(){
+        const startTime = new Date( window.localStorage.getItem("startTimer") || this.StartTimerHandler())
+        this.timerRef = window.setInterval(()=>{
+            const secsDiff = new Date().getTime() - startTime.getTime()
+            this.timer = this.secondToHms(Math.floor(secsDiff/1000))
+        }, 1000)
+    }
+
+secondToHms(d) {
+    d = Number(d);
+    const h = String(Math.floor(d / 3600)).padStart(2, '0');
+    const m = String(Math.floor((d % 3600) / 60)).padStart(2, '0');
+    const s = String(Math.floor(d % 60)).padStart(2, '0');
+    return `${h}:${m}:${s}`;
+}
+
+    connectedCallback(){
+        if(window.localStorage.getItem("startTimer")){
+            this.setTimer()
+        }
+    }
+}
